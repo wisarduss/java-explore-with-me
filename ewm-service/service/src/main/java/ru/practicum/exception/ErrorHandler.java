1a1409c2a1ddafb4c73c66c7eb76f2a35972d94c
Wisardus <maximus906544@yandex.ru>
2024-07-13 15:47:18 +0300

    add logging in ErrorHandler

--- ewm-service/service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ ewm-service/service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,6 +16,7 @@ import java.time.format.DateTimeFormatter;
 import static ru.practicum.utils.Constants.DATE_TIME_PATTERN;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
@@ -26,6 +28,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(IdNotFoundException.class)
     public ErrorResponse handleValidationExceptions(IdNotFoundException ex) {
+        log.debug("Получен код ошибки 404 not found");
         return ErrorResponse.builder()
                 .status(HttpStatus.NOT_FOUND.name())
                 .reason(OBJECT_NOT_FOUND_REASON)
@@ -37,6 +40,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(ConstraintViolationException.class)
     public ErrorResponse handleValidationExceptions(ConstraintViolationException ex) {
+        log.debug("Получен код ошибки 409 conflict");
         return ErrorResponse.builder()
                 .status(HttpStatus.CONFLICT.name())
                 .reason(CONFLICT_REASON)
@@ -48,6 +52,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(ConflictException.class)
     public ErrorResponse handleValidationExceptions(ConflictException ex) {
+        log.debug("Получен код ошибки 409 conflict");
         return ErrorResponse.builder()
                 .status(HttpStatus.CONFLICT.name())
                 .reason(UPDATE_CONFLICT_REASON)
@@ -59,6 +64,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        log.debug("Получен код ошибки 409 bad request");
         return ErrorResponse.builder()
                 .status(HttpStatus.BAD_REQUEST.name())
                 .reason(REQUEST_VALIDATION_REASON)
@@ -70,6 +76,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ErrorResponse handleTypeMismatchExceptions(RuntimeException ex) {
+        log.debug("Получен код ошибки 409 bad request");
         return ErrorResponse.builder()
                 .status(HttpStatus.BAD_REQUEST.name())
                 .reason(REQUEST_VALIDATION_REASON)
@@ -81,6 +88,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ValidationException.class)
     public ErrorResponse handleValidationExceptions(ValidationException ex) {
+        log.debug("Получен код ошибки 409 bad request");
         return ErrorResponse.builder()
                 .status(HttpStatus.BAD_REQUEST.name())
                 .reason(REQUEST_VALIDATION_REASON)
--- ewm-stats/stats-server/src/main/java/ru/practicum/stats/exception/ErrorHandler.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/stats/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.stats.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.validation.ValidationException;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ValidationException.class)
     public String handleValidationExceptions(ValidationException ex) {
+        log.debug("Получен код ошибки 409 bad request");
         return ex.getMessage();
     }
 }
